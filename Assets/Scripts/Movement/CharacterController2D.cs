41dc41d6b78275c1ca01ffd998448b1a96338084
Karhal <mar.fucik@gmail.com>
2020-05-02 16:13:01 +0200

    Player movement and physics. Cinemachine follow camera.

---
+++ Assets/Scripts/Movement/CharacterController2D.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameDevJam.Movement
+{
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class CharacterController2D : MonoBehaviour
+    {
+        [SerializeField] Collider2D groundChecker = null;
+        [SerializeField] LayerMask groundLayer = new LayerMask();
+
+        bool grounded = false;
+        bool isJumping = false;
+
+        Rigidbody2D myRigidbody;
+        Animator myAnimator;
+
+        private void Awake()
+        {
+            myRigidbody = GetComponent<Rigidbody2D>();
+            myAnimator = GetComponent<Animator>();
+        }
+
+        private void FixedUpdate()
+        {
+            grounded = false;
+
+            if (groundChecker.IsTouchingLayers(groundLayer))
+            {
+                grounded = true;
+            }
+        }
+
+        private void Update()
+        {
+            Flip();
+
+            myAnimator.SetFloat("moveSpeed", Mathf.Abs(myRigidbody.velocity.x));
+        }
+
+        public void Move(float moveSpeed, float moveSmoothing)
+        {
+            Vector2 targetvelocity = new Vector2(moveSpeed * 100, myRigidbody.velocity.y);
+            Vector2 velocity = Vector2.zero;
+
+            myRigidbody.velocity = Vector2.SmoothDamp(myRigidbody.velocity, targetvelocity, ref velocity, moveSmoothing);
+        }
+
+        public void Jump(float jumpForce)
+        {
+            if (grounded)
+            {
+                myRigidbody.AddForce(new Vector2(0f, jumpForce * 100));
+                isJumping = true;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        public bool GetIsJumping()
+        {
+            return isJumping;
+        }
+
+        private void Flip()
+        {
+            bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+            if (playerHasHorizontalSpeed)
+            {
+                transform.localScale = new Vector3(Mathf.Sign(myRigidbody.velocity.x), 1f, 1f);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GameDevJam.Movement;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [Range(0, .3f)]
+    [SerializeField] float moveSmoothing = 0.025f;
+    [SerializeField] float jumpForce = 5f;
+
+    CharacterController2D myController;
+
+    float horizontalMove = 0f;
+
+    private void Awake() 
+    {    
+        myController = GetComponent<CharacterController2D>(); 
+    }
+
+    private void FixedUpdate() 
+    {
+        Move();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void Move()
+    {
+        myController.Move(horizontalMove * Time.fixedDeltaTime, moveSmoothing);
+    }
+
+    private void Jump()
+    {
+        myController.Jump(jumpForce);
+    }
+}

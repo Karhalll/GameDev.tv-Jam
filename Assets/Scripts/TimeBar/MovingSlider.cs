42c5b4bfbe9e39f469f58a55ddd6528a2650a06f
Karhal <mar.fucik@gmail.com>
2020-05-05 19:38:42 +0200

    Flower sprites imported. Timebar moving.

---
+++ Assets/Scripts/TimeBar/MovingSlider.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class MovingSlider : MonoBehaviour
+{
+    [SerializeField] float period = 2f;
+
+    Slider mySlider;
+
+    float startingPosition;
+    float currentPosition;
+    float normTime;
+
+    private void Awake() 
+    {
+        mySlider = GetComponent<Slider>();
+    }
+
+    private void Start() 
+    {
+        startingPosition = mySlider.value;
+        currentPosition = startingPosition;
+        normTime = NormPos(startingPosition);
+    }
+    
+    void Update()
+    {
+        float currentNormPos = NormPos(currentPosition);
+        float cycles = NormPosCycle() / period;
+        float rawSin = Mathf.Sin(cycles * 2f * Mathf.PI - Mathf.PI/2);
+
+        float positiveSin = (rawSin + 1f) / 2f;
+
+        float newValue = mySlider.maxValue * positiveSin;
+
+        mySlider.value = newValue;
+        currentPosition = newValue;
+    }
+
+    public void SetStartingPos(float startingPos)
+    {
+        startingPosition = startingPos;
+    }
+
+    public float GetCurrentPos()
+    {
+        return currentPosition;
+    }
+
+
+    private float NormPosCycle()
+    {
+        float newNormPos = normTime + Time.deltaTime;
+        
+        normTime = newNormPos;
+        return newNormPos;
+    }
+
+    private float NormPos(float position)
+    {
+        return (position / mySlider.maxValue); 
+    }
+
+
+}
---
+++ Assets/Scripts/TimeBar/TimeBar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameDevJam.TimeBar
+{
+    public class TimeBar : MonoBehaviour
+    {
+        [SerializeField] float maxTimeToManip = 1000f;
+        [SerializeField] int maxCystals = 5;
+        [SerializeField] int crystalsOwned = 2;
+
+        [SerializeField] Slider activeBarFill = null;
+        [SerializeField] Slider movingSlider = null;
+
+        float currenTimeToManip;
+
+        private void Awake() 
+        {
+            currenTimeToManip = (maxTimeToManip / maxCystals) * crystalsOwned;
+        }
+
+        private void Start() 
+        {
+            activeBarFill.maxValue = maxTimeToManip;
+            activeBarFill.value = currenTimeToManip;
+
+            movingSlider.maxValue = currenTimeToManip;
+        }
+    }
+}

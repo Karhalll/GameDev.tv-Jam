5dada0d650c6d274ac7d4a5009a63d424e87a4d5
Martin Fučík <mar.fucik@gmail.com>
2020-05-29 17:29:05 +0200

    Added water with spawn point

---
+++ Assets/Scripts/Enviroment/Water.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameDevJam.Enviroment
+{
+  public class Water : MonoBehaviour
+  {
+    [SerializeField] Transform respawnPoint = null;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            other.gameObject.transform.position = respawnPoint.position;
+        }
+    }
+  }
+}
+

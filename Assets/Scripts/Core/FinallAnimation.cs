9327d8556f6293434efcfe9e75bd359b34c7a2cc
Martin Fučík <mar.fucik@gmail.com>
2020-05-31 12:44:01 +0200

    Added mollo. Finall animation preparation.

---
+++ Assets/Scripts/Core/FinallAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+using GameDevJam.Controls;
+
+namespace GameDevJam.Core
+{
+  public class FinallAnimation : MonoBehaviour
+  {
+	[SerializeField] PlayableDirector directour = null;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player")
+		{
+			PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
+			playerController.DisableControl();
+
+			directour.Play();
+		}
+    }
+  }
+
+}
